b4bdae393b3a99c00c11ba0b174618c24f596474
VictorSarov <victor.sarov@rentalmatics.com>
2026-06-01 09:59:03 +0100

    good

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'king_kong': '10rem',
+      },
+      colors: {
+        'super-pink': 'pink'
+      },
+      fontFamily: {
+        'my_font': ['"Black Ops One"', 'cursive']
+      }
+    },
   },
   variants: {
     extend: {},

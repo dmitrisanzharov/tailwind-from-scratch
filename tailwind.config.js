6483504e3197c09d6cdf34a84ab67f94b9fd18b6
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-17 08:47:57 +0100

    all good

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {
       fontSize: {
         "massive": "10rem",
+      },
+      colors: {
+        "wacky_color": "#ff0000",
+      },
+      fontFamily: {
+        'yuyu': ["Yuyu Short", "sans-serif"],
       }
     },
   },
